5f4da4caf3a2e3bd72f434392d35a7345723cf49
Sorikairo <sorikairox@gmail.com>
2023-06-12 20:55:43 +0900

    chore(testbundle): add import map path

--- back/testbundle.ts
+++ back/testbundle.ts
@@ -1,6 +1,7 @@
 import { bundle } from "https://deno.land/x/emit@0.24.0/mod.ts";
 const result = await bundle(new URL("./run.ts", import.meta.url), {
   compilerOptions: { emitDecoratorMetadata: true },
+  importMap: "../import_map.json",
 });
 
 const { code } = result;
